b7df2fe7cf3585f8f94047e0eb78d38571166afc
andreluizpo <andreluizoliveira163@hotmail.com>
2026-05-12 20:59:55 -0400

    Create 2026-05-12_Character_Frequency.js

---
+++ 2026/2026-05/2026-05-12_Character_Frequency.js
@@ -0,0 +1,27 @@
+/*
+freeCodeCamp - Daily Coding Challenges (05/12/2026) 
+
+**Character Frequency**
+
+Given a string, return an object (JavaScript) or dictionary (Python) mapping each character to the number of times it appears.
+
+*/
+
+function getFrequency(str) {
+  let obj = {};
+
+  for (const char of str) {
+    if (!(char in obj)) {
+      obj[char] = 0;
+    }
+    obj[char]++;
+  }
+
+  return obj;
+}
+
+getFrequency("test"); // should return {t: 2, e: 1, s: 1}.
+getFrequency("mississippi"); // should return {m: 1, i: 4, s: 4, p: 2}.
+getFrequency("hello world"); // should return {h: 1, e: 1, l: 3, o: 2, " ": 1, w: 1, r: 1, d: 1}.
+getFrequency("She sells seashells by the seashore."); // should return {S: 1, h: 4, e: 7, " ": 5, s: 7, l: 4, a: 2, b: 1, y: 1, t: 1, o: 1, r: 1, ".": 1}.
+getFrequency("The quick brown fox jumps over the lazy dog."); // should return {T: 1, h: 2, e: 3, " ": 8, q: 1, u: 2, i: 1, c: 1, k: 1, b: 1, r: 2, o: 4, w: 1, n: 1, f: 1, x: 1, j: 1, m: 1, p: 1, s: 1, v: 1, t: 1, l: 1, a: 1, z: 1, y: 1, d: 1, g: 1, ".": 1}.
