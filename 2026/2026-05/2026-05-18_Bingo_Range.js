76f97838ce474248f1894e03fd77d49a409a675f
andreluizpo <andreluizoliveira163@hotmail.com>
2026-05-18 22:28:14 -0400

    Create 2026-05-18_Bingo_Range.js

---
+++ 2026/2026-05/2026-05-18_Bingo_Range.js
@@ -0,0 +1,39 @@
+/*
+freeCodeCamp - Daily Coding Challenges (2026-05-17) 
+
+**Bingo Range**
+
+Given a bingo letter, return the number range associated with that letter.
+
+Letter	Number Range
+"B"	1-15
+"I"	16-30
+"N"	31-45
+"G"	46-60
+"O"	61-75
+
+Return an array with all numbers in the range from smallest to largest.
+*/
+
+function getBingoRange(letter) {
+  let numberRange = [];
+  let maxValues = {
+    B: 15,
+    I: 30,
+    N: 45,
+    G: 60,
+    O: 75,
+  };
+
+  for (let num = maxValues[letter] - 14; num <= maxValues[letter]; num++) {
+    numberRange.push(num);
+  }
+
+  return numberRange;
+}
+
+getBingoRange("B"); // should return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].
+getBingoRange("I"); // should return [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30].
+getBingoRange("N"); // should return [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45].
+getBingoRange("G"); // should return [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60].
+getBingoRange("O"); // should return [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75].
